88e64819b1f288870639fac1518bc49730cc3160
donBarbos <donbarbos@proton.me>
2024-12-30 04:11:36 +0400

    feat: add scroll-based visibility to Header and ScrollToTop components
    - Updated Header component to hide on downward scroll and show on upward scroll
    - Modified ScrollToTop button to appear only when scrolling upward and past a threshold
    - Added `.hidden` class for Header to manage transform transitions
    - Refactored ScrollToTop styles for better visibility transitions and maintainability

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -3,38 +3,38 @@ import { useEffect, useState } from 'react'
 import styles from './styles.module.css'
 
 export const ScrollToTop = () => {
-  const [showButton, setShowButton] = useState<boolean>(false)
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+  const [lastScrollY, setLastScrollY] = useState<number>(0)
 
   useEffect(() => {
-    let lastScrollPos = 0
-    const handleScrollEvent = () => {
-      const currentScrollPos = window.pageYOffset
-      if (currentScrollPos > lastScrollPos + 10) {
-        setShowButton(false)
-      } else if (currentScrollPos < lastScrollPos - 10 && currentScrollPos > 100) {
-        setShowButton(true)
-      }
-      lastScrollPos = currentScrollPos
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
 
-      if (currentScrollPos === 0) {
-        setShowButton(false)
+      if (currentScrollY < lastScrollY && currentScrollY > 50) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
       }
+
+      setLastScrollY(currentScrollY)
     }
-    window.addEventListener('scroll', handleScrollEvent)
-    return () => window.removeEventListener('scroll', handleScrollEvent)
-  }, [setShowButton])
 
-  const handleClick = () => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [lastScrollY])
+
+  const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }
 
   return (
     <button
-      className={showButton ? `${styles.top_link} ${styles.active}` : `${styles.top_link}`}
-      onClick={handleClick}
+      className={`${styles.top_link} ${isVisible ? styles.visible : ''}`}
+      onClick={scrollToTop}
       title="Scroll to top"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="black" role="presentation">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" role="presentation">
         <path
           fillRule="evenodd"
           d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
